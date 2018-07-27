eb9bbd93eebfaee5341755709d1685bc831bc845
linzhun <linzhun7@gmail.com>
2018-07-27 10:59:55 +0800

    pandas 操作

--- util.py
+++ util.py
@@ -511,6 +511,9 @@ roc_auc_score(ytest, p)
 print_graph(t1, xtrain.columns)
 
 #-----------pandas 常用代码
+# 用字典 创建DataFrame
+pd.DataFrame.from_dict(dict, orient='index').T
+
 X = X.loc[(X < 0).sum(1) == 0]  # 不含缺失数据的行（如-1， -2 值）
 df.loc[(df > 0.2).any(1)]
 
@@ -524,6 +527,15 @@ for name, group in gb:
 y = 1 * (df.cand_pty_affiliation == 'REP') # 将 label = 'REP' 的改为1，否则为0
 
 # 删除只有1个值的列
-X.drop(X.columns[X.std() == 0], axis=1, inplace=True)
+def drop1(df):
+    onecols = df.columns[df.std() == 0]
+    df.drop(onecols, axis=1, inplace=True)
+    return df
+    
+# pandas 处理文本数据
+s.str.split(',')
+s.str.extract？
 
+# pandas 批量更改列名 .columns.get_level_values
+f4.columns = f4.columns.get_level_values(0) + 'ctime_'
 
